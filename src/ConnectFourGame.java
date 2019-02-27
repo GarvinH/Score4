7e34437fbd5b993091edc6418854d7ff658c6e0f
AlbertQuon <albertq2001@hotmail.com>
2019-02-27 09:06:38 -0500

    Add files via upload

---
+++ src/ConnectFourGame.java
@@ -0,0 +1,273 @@
+package recursion;
+import java.util.Scanner;
+import java.util.Random;
+
+public class ConnectFourGame {
+
+    public static void main(String[] args) {
+        int boardSize = 4; // for loops will have grid.length
+        int[][][] grid = new int[10][10][10];
+        int[][][] testGrid = {
+                {       {1,0,0,0},//floor 1
+                        {0,0,0,0},
+                        {0,0,0,0},
+                        {0,0,0,0}},
+
+                {       {0,0,0,0},//floor 2
+                        {0,0,0,0},
+                        {1,0,0,0},
+                        {0,0,0,0}},
+
+                {       {0,0,0,0},//floor 3
+                        {1,0,0,0},
+                        {0,0,0,0},
+                        {0,0,0,0}},
+
+                {       {1,0,0,0}, //floor4
+                        {0,0,0,0},
+                        {0,0,0,0},
+                        {0,0,0,0}}
+        };
+        //int[] gridIndexes = new int[3];
+        System.out.println(win(testGrid));
+        randomMove(testGrid);
+        // i = height
+        // j = row
+        // k = column
+//        for (int i = 0; i < grid.length; i++) {
+//            System.out.println("Floor " + (i + 1));
+//            for (int j = 0; j < grid.length; j++) {
+//                for (int k = 0; k < grid.length; k++) {
+//                    System.out.print(testGrid[i][j][k]);
+//                }
+//                System.out.println("");
+//            }
+//            System.out.println("");
+//        }
+    }
+    // loop through the grid
+    static void randomMove(int[][][] grid) {
+        Random rand = new Random();
+        int randX;
+        int randY;
+        boolean floorClear = false;
+        boolean played = false;
+        for (int i = 0; i < grid.length; i++) {
+            //System.out.println("Floor " + (i + 1));
+            for (int j = 0; j < grid.length; j++) {
+                for (int k = 0; k < grid.length; k++) {
+                    if (grid[i][j][k] == 0) {
+                        floorClear = true;
+
+                    }
+                    //System.out.print(grid[i][j][k]);
+                }
+                //System.out.println("");
+            }
+            while (!played && floorClear) {
+                randX = rand.nextInt(4);
+                randY = rand.nextInt(4);
+                if (grid[i][randX][randY] == 0) {
+                    grid[i][randX][randY] = 1;
+                    played = true;
+                }
+            }
+            //System.out.println("");
+        }
+
+    }
+    static void findValues(int[][][] grid) {
+
+
+    }
+
+    // record the number of possibilities to win
+    // alternate between player A and B (A prefers highest, B prefers lowest)
+    // return the optimal move
+    static void turn (int[][][] grid, int currentPlayer, int[] gridIndex, int score) {
+
+
+    }
+    // go through each row
+    // go through each column
+    // go through height
+    // ex. [0][0][0] = [0][0][1]
+    // go through diagonally on the same plane
+    // go through diagonally on each separate individual plane
+    // ex. [0][0][0] == [1][1][1] or [0][0][0] == [1][0][1] or [0][0][0] == [0][1][1]
+    static int win(int[][][] grid) {
+        int checkC, checkH, checkR, checkDiag,
+                checkDiagRowUp, checkDiagColUp,
+                checkDiagRowDown, checkDiagColDown,
+                checkFloorsUp, checkFloorsDown, checkWin;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                for (int k = 0; k < grid.length; k++) {
+                    checkR = winCheck(grid, j, k, i, 0, 0);
+                    checkC = winCheck(grid, j, k, i, 0, 1);
+                    checkH = winCheck(grid, j, k, i, 0, 2);
+                    checkDiag = winCheck(grid, j, k, i, 0, 3);
+                    checkDiagRowUp = winCheck(grid, j, k, i, 0, 4);
+                    checkDiagColUp = winCheck(grid, j, k, i, 0, 5);
+                    checkFloorsUp = winCheck(grid, j, k, i, 0, 6);
+
+                    if((checkR == 1) || (checkR == -1)) {
+                        return checkR;
+                    }
+                    if((checkC == 1) || (checkC == -1)) {
+                        return checkC;
+                    }
+                    if((checkH == 1) || (checkH == -1)) {
+                        return checkH;
+                    }
+                    if((checkDiag == 1) || (checkDiag == -1)) {
+                        return checkDiag;
+                    }
+                    if((checkFloorsUp == 1) || (checkFloorsUp == -1)) {
+                        return checkFloorsUp;
+                    }
+                    if((checkDiagRowUp == 1) || (checkDiagRowUp == -1)) {
+                        return checkDiagRowUp;
+                    }
+                    if((checkDiagColUp == 1) || (checkDiagColUp == -1)) {
+                        return checkDiagColUp;
+                    }
+                    if (i >= 3) {
+
+                        checkDiagRowDown = winCheckDown(grid, j, k, i, 0, 0);
+                        checkDiagColDown = winCheckDown(grid, j, k, i, 0, 1);
+                        checkFloorsDown = winCheckDown(grid, j, k, i, 0, 2);
+                        if ((checkFloorsDown == 1) || (checkFloorsDown == -1)) {
+                            return checkFloorsDown;
+                        }
+                        if ((checkDiagColDown == 1) || (checkDiagColDown == -1)) {
+                            return checkDiagColDown;
+                        }
+                        if ((checkDiagRowDown == 1) || (checkDiagRowDown == -1)) {
+                            return checkDiagRowDown;
+                        }
+                    }
+                    checkWin = checkR + checkC + 
+
+                }
+            }
+        }
+
+        return 0;
+    }
+
+
+
+    /**
+     * Checks all win conditions and see if it's valid for any player
+     * @param grid
+     * @param row
+     * @param column
+     * @param height
+     * @param target
+     * @param type Determines where the method should search
+     * @return
+     */
+    static int winCheck(int[][][] grid, int row, int column, int height, int target, int type) {
+        if ((target == 6) || (target == -6)) {
+            return target/4;
+        }
+        if ((row == grid.length-1) || (column == grid.length-1) || (height == grid.length-1)) {
+            return 0;
+        }
+        // check row
+        if ((grid[height][row][column] == grid[height][row+1][column]) && (grid[height][row][column] != 0)
+                && (type == 0)) {
+            return (winCheck(grid, row+1, column, height,
+                    target+grid[height][row][column]+grid[height][row+1][column], 0) +
+                    winCheck(grid, row+1, column, height, target, 0));
+        }
+        // check column
+        if ((grid[height][row][column] == grid[height][row][column+1]) && (grid[height][row][column] != 0)
+                && (type == 1)) {
+            return (winCheck(grid, row, column+1, height,
+                    target+grid[height][row][column]+grid[height][row][column+1], 1) +
+                    winCheck(grid, row, column+1, height, target, 1));
+        }
+        // check height
+        if ((grid[height][row][column] == grid[height+1][row][column]) && (grid[height][row][column] != 0)
+                && (type == 2)) {
+            return (winCheck(grid, row, column, height+1,
+                    target+grid[height][row][column]+grid[height+1][row][column], 2) +
+                    winCheck(grid, row, column, height+1, target, 2));
+        }
+        // check diagonal on the same plane
+        if ((grid[height][row][column] == grid[height][row+1][column+1]) && (grid[height][row][column] != 0)
+                && (type == 3)) {
+            return (winCheck(grid, row+1, column+1, height,
+                    target+grid[height][row][column]+grid[height][row+1][column+1], 3) +
+                    winCheck(grid, row+1, column+1, height, target, 3));
+        }
+        // check diagonal heights on same column
+        if ((grid[height][row][column] == grid[height+1][row+1][column]) && (grid[height][row][column] != 0)
+                && (type == 4)) {
+            return (winCheck(grid, row+1, column, height+1,
+                    target+grid[height][row][column]+grid[height+1][row+1][column], 4) +
+                    winCheck(grid, row+1, column, height+1, target, 4));
+        }
+        // check diagonal heights on the same row
+        if ((grid[height][row][column] == grid[height+1][row][column+1]) && (grid[height][row][column] != 0)
+                && (type == 5)) {
+            return (winCheck(grid, row, column, height+1,
+                    target+grid[height][row][column]+grid[height+1][row][column+1], 5) +
+                    winCheck(grid, row+1, column, height+1, target, 5));
+        }
+        // check diagonal on increasing floors across
+        if ((grid[height][row][column] == grid[height+1][row+1][column+1]) && (grid[height][row][column] != 0)
+                && (type == 6)) {
+            return (winCheck(grid, row+1, column+1, height+1,
+                    target+grid[height][row][column]+grid[height+1][row+1][column+1], 6) +
+                    winCheck(grid, row+1, column+1, height+1, target, 6));
+        }
+
+        return 0;
+
+    }
+
+    /**
+     * Fix this method
+     * @param grid
+     * @param row
+     * @param column
+     * @param height
+     * @param target
+     * @param type
+     * @return
+     */
+    static int winCheckDown(int[][][] grid, int row, int column, int height, int target, int type) {
+        System.out.println(target);
+        if ((target == 6) || (target == -6)) {
+            return target/4;
+        }
+        if ((row == 0) || (column == 0) || (height == 0)) {
+            return 0;
+        }
+        //check diagonally downwards on same row
+        if ((grid[height][row][column] == grid[height-1][row][column-1]) && (grid[height][row][column] != 0)
+                && (type == 0)) {
+            return (winCheck(grid, row, column-1, height-1,
+                    target+grid[height][row][column]+grid[height-1][row][column-1], 0) +
+                    winCheck(grid, row, column-1, height-1, target, 0));
+        }
+        //check diagonally downwards on same column
+        if ((grid[height][row][column] == grid[height-1][row-1][column]) && (grid[height][row][column] != 0)
+                && (type == 1)) {
+            return (winCheck(grid, row-1, column, height-1,
+                    target+grid[height][row][column]+grid[height-1][row-1][column], 1) +
+                    winCheck(grid, row-1, column, height-1, target, 1));
+        }
+        //check diagonally downwards across
+        if ((grid[height][row][column] == grid[height-1][row-1][column-1]) && (grid[height][row][column] != 0)
+                && (type == 0)) {
+            return (winCheck(grid, row-1, column-1, height-1,
+                    target+grid[height][row][column]+grid[height-1][row-1][column-1], 2) +
+                    winCheck(grid, row-1, column-1, height-1, target, 2));
+        }
+        return 0;
+    }
+}
